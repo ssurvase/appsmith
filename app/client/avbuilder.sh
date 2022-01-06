c9c84c35b81655035ee489dd28ac0772c520d42e
Jerry M <developer.jerrymannel@gmail.com>
2022-01-06 22:34:18 +0530

    Fixed build script

--- app/client/avbuilder.sh
+++ app/client/avbuilder.sh
@@ -2,11 +2,22 @@
 
 echo "Starting build"
 
-docker build . -t avtool_client
+TAG=$(date +%Y.%m.%d.%H.%M)
 
-echo "scaling the pods"
-kubectl scale deployment appsmith-editor --replicas=0
-kubectl scale deployment appsmith-editor --replicas=1
+echo "Using tag :: $TAG"
+
+sed -i.bak s#__image_tag__#$TAG# Dockerfile
+
+echo "docker build . -t avtool_client:$TAG"
+docker build . -t avtool_client:$TAG
+
+echo "Applying image to k8s deployment appsmith-editor"
+echo "kubectl set image -n default --record=true deployment/appsmith-editor appsmith-editor=avtool_client:$TAG"
+kubectl set image -n default --record=true deployment/appsmith-editor appsmith-editor=avtool_client:$TAG
+
+# echo "scaling the pods"
+# kubectl scale deployment appsmith-editor --replicas=0
+# kubectl scale deployment appsmith-editor --replicas=1
 
 echo "fin"
 
