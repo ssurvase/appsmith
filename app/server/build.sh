6385fb05c8ff7e892ecc37995758883decdfa4b6
Jerry M <developer.jerrymannel@gmail.com>
2022-01-22 02:48:23 +0530

    Trying oracle plugin development

--- app/server/build.sh
+++ app/server/build.sh
@@ -4,6 +4,7 @@
 rm -rf dist/
 
 # Build the code. $@ accepts all the parameters from the input command line and uses it in the maven build command
+# mvn clean package "$@"
 mvn clean package "$@"
 
 if [ $? -eq 0 ]
@@ -22,3 +23,7 @@ cp -v ./appsmith-server/target/server-*.jar dist/
 
 # Copy all the plugins
 rsync -av --exclude "original-*.jar" ./appsmith-plugins/*/target/*.jar dist/plugins/
+
+TAG=$(date +%Y.%m.%d.%H.%M)
+
+docker build -t av-server:${TAG} .
